340ab7b5c54d4b0bbb2e08d534e71f588e3792d1
Quincy West <rakaneth@gmail.com>
2018-08-19 23:37:26 -0400

    initial testing

--- vtermtest/src/main/java/com/rakaneth/roguelikes/vtermtest/App.java
+++ vtermtest/src/main/java/com/rakaneth/roguelikes/vtermtest/App.java
@@ -1,13 +1,23 @@
 package com.rakaneth.roguelikes.vtermtest;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+
+import com.valkryst.VTerminal.Screen;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        final Screen screen = new Screen();
+        screen.addCanvasToFrame();
+        for (int x=0; x<10; x++) 
+        {
+        	for (int y=0; y<10; y++) 
+        	{
+        		screen.getTileAt(x, y).setCharacter('x');
+        	}
+        }
+        
+        screen.draw();
     }
 }
